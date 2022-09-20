ac466bae8209779256d3c29f66182333d7424185
jpecora716 <6646235+jpecora716@users.noreply.github.com>
2022-09-20 16:51:48 -0400

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -62,6 +62,16 @@ resource "aws_s3_bucket" "financials" {
 
 }
 
+resource "aws_s3_bucket" "not-financials" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-not-financials"
+  acl           = "private"
+  force_destroy = true
+
+}
+
 resource "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs
